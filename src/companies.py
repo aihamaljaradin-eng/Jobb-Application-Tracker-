548c384dd9c8fa79fa531e689c68090b776e9b4d
Aiham Aljaradin <aihamaljaradin@gmail.com>
2026-04-06 10:44:56 +0200

    Implement list_companies function
    Add function to list companies from the database

--- src/companies.py
+++ src/companies.py
@@ -1,1 +1,16 @@
+from utils import hr, pause
 
+def list_companies(conn):
+    cursor = conn.cursor()
+    hr()
+    print("COMPANIES")
+    hr()
+
+    cursor.execute("SELECT * FROM Company")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    pause()
